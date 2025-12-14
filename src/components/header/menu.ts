9e0d3fc81805557dc60ce77b8f8c149ed08ea7e3
ngrwisnu <adiprayogongrwisnu@gmail.com>
2025-12-14 08:17:27 +0700

    fix href on contact link

--- src/components/header/menu.ts
+++ src/components/header/menu.ts
@@ -1,13 +1,13 @@
 export const desktopMenu = [
   { name: "Home", href: "/" },
   { name: "Works", href: "/#works" },
-  { name: "Contact", href: "/#contact" },
+  { name: "Contact", href: "#contact" },
 ];
 
 export const mobileMenu = [
   { name: "Home", href: "/" },
   { name: "Works", href: "/#works" },
-  { name: "Contact", href: "/#contact" },
+  { name: "Contact", href: "#contact" },
   {
     name: "Resume",
     href: "/assets/docs/CV_Ngr-Wisnu-Adiprayogo.pdf",
